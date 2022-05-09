322c6d591782542503070fa4aa0dfc916fda4200
Roi Argaman <zebroi.arg@gmail.com>
2022-05-09 14:28:09 +0300

    hm2 vs florist

---
+++ hm2/hm2/Person.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <vector>
+#include <Florist.h>
+
+class Person
+{
+protected:
+	std::string name;
+public:
+	void orderFlowers(Florist*,Person*, std::vector < std::string>)
+	{
+		std::cout << "orderFlowers Not emplimented yet" << std::endl;
+	}
+
+		{
+		void acceptFlowers(FlowersBouquet)
+		{
+			std::cout << "acceptFlowers Not emplimented yet" << std::endl;			
+		}
+
+};
