4e35bfea1a790f762a8ae0b67086944da2525ba3
shakedmatityahu <matshake@post.bgu.ac.il>
2022-05-13 11:23:28 +0300

    shaked- changes in florist, person

--- hm2/hm2/Florist.h
+++ hm2/hm2/Florist.h
@@ -1,5 +1,14 @@
 #pragma once
-class Florist
+#include "Person.h"
+
+class Florist :  public  Person 
 {
-};
+private:
+	Wholesaler* : wholesaler;
+	//Arranger* : arranger;
+	//DeliveryPerson* : deliveryPerson;
+			
+public:
+	Florist (std:: string name , Wholesaler* wholesaler, Arranger* arranger )
+	void acceptOrder(Person* p, std::vector<std::string>);
 
---
+++ hm2/hm2/Wholesaler.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Person.h"
+class Wholesaler : public Person
+{
+	int acceptOrder(std:: vector <std :: string>);
+};
+
