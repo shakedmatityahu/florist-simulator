073979872548b53b8c89d27277096daf04016757
shakedmatityahu <matshake@post.bgu.ac.il>
2022-05-13 11:31:22 +0300

    shaked  more chnages

--- hm2/hm2/Florist.h
+++ hm2/hm2/Florist.h
@@ -1,14 +1,16 @@
 #pragma once
 #include "Person.h"
+#include "Wholesaler.h"
 
-class Florist :  public  Person 
+class Florist : public  Person
 {
 private:
-	Wholesaler* : wholesaler;
-	//Arranger* : arranger;
-	//DeliveryPerson* : deliveryPerson;
-			
+	Wholesaler* wholesaler;
+	//Arranger*  arranger;
+	//DeliveryPerson*  deliveryPerson;
+
 public:
-	Florist (std:: string name , Wholesaler* wholesaler, Arranger* arranger )
+	Florist(std::string name, Wholesaler* wholesaler);
 	void acceptOrder(Person* p, std::vector<std::string>);
 
+};
